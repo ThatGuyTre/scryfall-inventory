169a5e9942248c89728b6e867a8f706cdc8db9e8
ThatGuyTre <trebug123@gmail.com>
2023-09-23 10:51:48 -0500

    Trying to make index

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from '@chakra-ui/react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Button>Welcome to the Next.js App</Button>
+      {/* Your content here */}
+    </div>
+  );
+};
+
+export default HomePage;
