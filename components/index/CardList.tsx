a8f38d71a7cbc8c3003f6ffe975ab9b1ad21994d
ThatGuyTre <trebug123@gmail.com>
2023-09-23 21:56:53 -0500

    Fixed double render and cardlist loading

--- components/index/CardList.tsx
+++ components/index/CardList.tsx
@@ -1,68 +1,53 @@
 import { Flex, Stack } from "@chakra-ui/react";
 import Card from "./Card";
 import { GameCardProps, fetchRandomCard } from "../ScryfallAPI";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-const cards: GameCardProps[] = [
-	{
-		title: "Loading...",
-		description: "Please wait while the cards are loaded.",
-		imageSrc: "",
-		imageAlt: "",
-		key: "loading",
-	}
-];
+interface GameGridProps {}
 
-const fetchPromises: Promise<any>[] = [];
+export default function CardList({}: GameGridProps) {
+  const [cards, setCards] = useState<GameCardProps[]>([]); // Initialize cards as an empty array
+  const [isLoaded, setIsLoaded] = useState<boolean>(false); // Initialize isLoaded as false
 
-function populateCards() {
-	for (let i = 0; i < 5; i++) {
-		fetchPromises.push(fetchRandomCard());
-	  }
-	  
-	  Promise.all(fetchPromises).then((formattedCards) => {
-		  // All fetch operations have completed successfully.
-		  // formattedCards is an array of card data.
-			cards.pop(); // Remove the loading card
-			
-			formattedCards.forEach((formattedCard) => {
-				if (formattedCard !== null) {
-					cards.push(formattedCard);
-				}
-			});
-		  
-		  // Now 'cards' contains the cards that were successfully fetched.
-		  console.log(cards);
-		}).catch((error) => {
-		  // Handle errors here
-		  console.error("Error fetching random cards:", error);
-		});
-}
+  useEffect(() => {
+    const fetchCards = async () => {
+      try {
+        const fetchPromises = [];
 
-useEffect(() => {
-	populateCards();
-}, []);
+        // Call ScryfallRandomCard 15 times to get 15 random cards
+        for (let i = 0; i < 10; i++) {
+          fetchPromises.push(fetchRandomCard());
+        }
 
-interface GameGridProps {
-}
+        const formattedCards = await Promise.all(fetchPromises);
 
-export default function CardList({ }: GameGridProps) {
+        // Filter out any null values
+        const validCards = formattedCards.filter((formattedCard) => formattedCard !== null);
 
-	return(
-		
-			<Flex maxW="3600" m="auto" justify="center" direction="row">
-				<Stack flex="1" direction="column" alignItems="center" ml="1vh" mr="1vh" mb="4vh" mt="1vh" rowGap="2">
-					{cards.map((card) => {
-						return <Card
-							key={card.key} // This is required for mapped objects but is not used
-							title={card.title}
-							description={card.description}
-							imageSrc={card.imageSrc}
-							imageAlt={card.imageAlt}
-						/>;
-					}
-					)}
-				</Stack>
-			</Flex>
-	);
+        // Update the cards state with the fetched and filtered cards
+        setCards((prevCards) => [...prevCards, ...validCards]);
+        setIsLoaded(true); // Set isLoaded to true when the cards are loaded
+      } catch (error) {
+        // Handle errors here
+        console.error("Error fetching random cards:", error);
+      }
+    };
+
+    fetchCards();
+  }, []); // Empty dependency array ensures this effect runs only once on component mount
+
+  return (
+    <Flex maxW="3600" m="auto" justify="center" direction="row">
+      <Stack flex="1" direction="column" alignItems="center" ml="1vh" mr="1vh" mb="4vh" mt="1vh" rowGap="2">
+        {isLoaded ? ( // Check if cards are loaded
+          cards.map((card) => {
+            console.log("displaying a card"); // This is just to show that the code is running
+            return <Card key={card.key} title={card.title} description={card.description} imageSrc={card.imageSrc} imageAlt={card.imageAlt} />;
+          })
+        ) : (
+          <p>Loading...</p> // Display a loading message while cards are being fetched
+        )}
+      </Stack>
+    </Flex>
+  );
 }
